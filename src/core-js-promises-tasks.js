95378f801e0bcb78538ebfeb0ad13bb7da4989ab
Mineclinee <vasily-minecline@yandex.ru>
2024-04-15 13:54:09 +0300

    feat: add the last one task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -146,8 +146,18 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let resultPromise = Promise.resolve('');
+
+  promises.forEach((currentPromise) => {
+    resultPromise = resultPromise.then((currentResult) => {
+      return currentPromise.then((value) => {
+        return currentResult + value;
+      });
+    });
+  });
+
+  return resultPromise;
 }
 
 module.exports = {
